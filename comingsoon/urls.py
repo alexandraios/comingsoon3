bdf44452dba6b215532007320b31904cab4a8327
Alexandra Rios <miss.alexrios@gmail.com>
2019-02-20 09:30:57 -0500

    no jobs file, add static

--- comingsoon/urls.py
+++ comingsoon/urls.py
@@ -21,5 +21,5 @@ from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('alex', jobs.views.alex, name='alex'),
+    path('alex', static.views.alex, name='alex'),
 ]
