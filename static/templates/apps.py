e0fcbba61c3f2c74e016f0ee6aade3dfd3396d44
Alexandra Rios <miss.alexrios@gmail.com>
2019-02-20 09:34:13 -0500

    mispelling

--- static/templates/apps.py
+++ static/templates/apps.py
@@ -2,4 +2,4 @@ from django.apps import AppConfig
 
 
 class TempmplatesConfig(AppConfig):
-    name = 'tempmplates'
+    name = 'templates'
--- static/templates/views.py
+++ static/templates/views.py
@@ -1,3 +1,5 @@
 from django.shortcuts import render
 
 # Create your views here.
+def alex(request):
+    return render(request, 'static/alex.html')
